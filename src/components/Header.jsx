a86beb3f8df7b3e479135f02d212a37e34ea75d5
hamyeonsu <iddustn1@naver.com>
2024-07-29 12:11:01 +0900

    1_header(완성)
    1_header(완성)

---
+++ src/components/Header.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+
+const headerNav =[
+  {
+    title:"intro",
+    url:"#intro",
+
+    },
+    {
+      title:"skill",
+      url:"#skill",
+  
+      },
+      {
+        title:"site",
+        url:"#site",
+    
+        },
+        {
+          title:"port",
+          url:"#port",
+      
+          },
+          {
+            title:"contact",
+            url:"#contact",
+        
+            },
+        
+]
+
+const Header = () => {
+
+  const [show,setShow] = useState(false);
+  const toggleMenu = () =>{
+    setShow((prevShow) => !prevShow);
+    // ! : 반대 ( 반대로 바꿔주는 명령어)
+
+  }
+  
+  
+
+
+  return (
+    <header id='header' role='banner'>
+      <div className="header_inner">
+        <div className="header_logo">
+          <a href="/">portfolio <em>react</em></a>
+        </div>
+        <div className={`header_nav ${show ? "show" :""}`} role='navigation' aria-label='메인 메뉴'>
+
+          <ul>
+
+            {headerNav.map((nav,key)=>(
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+                </li>
+
+            ))}
+            
+
+
+
+            
+          </ul>
+        </div>
+        <div className="header_nav_moblie" id="headerToggle" aria-controls="primany-menu" aria-expanded={show ? "true":"false"} role='button' tabIndex="0" onClick={toggleMenu}>
+          <span></span>
+
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
