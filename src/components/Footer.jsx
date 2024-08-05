33d9c7b21cbf20752d0fabe53e849ac0936e41f0
hamyeonsu <iddustn1@naver.com>
2024-08-05 13:12:14 +0900

    완성
    완성본

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer id='footer' role='contentinfo'>
+      <div className="footer_inner">
+        <div className="footer_text">
+        <span>HAMYEONSU</span>
+        <span>H.P:010-3912-7952</span>
+
+        </div>
+        <div className="footer_info">
+          <div className="left">
+            <div className="title">
+              <a href="/">포트폴리오 마무리영역</a>
+            </div>
+            <p>그동안 너무들 고생하셨습니다.</p>
+          </div>
+          <div className="right">
+            <h3>목록</h3>
+            <ul>
+              <li>
+                <a href="/">1차 포트폴리오</a>
+              <em>1차 포트폴리오 내용 기입</em>
+              </li>
+              <li>
+                <a href="/">팀프로젝트</a>
+              <em>팀프로젝트 내용 기입</em>
+              </li>
+              <li>
+                <a href="/">2차포트폴리오</a>
+              <em>2차 포트폴리오 내용기입</em>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer_bottom">
+          ⓒ &copy; 2024 daifu<br/>
+          개인 포트폴리오입니다. 상업목적으로 사용하지 않습니다. 
+        </div>
+       
+      </div>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,68 @@
 import React, { useState } from 'react'
-
-const headerNav =[
-  {
-    title:"intro",
-    url:"#intro",
-
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
     },
     {
-      title:"skill",
-      url:"#skill",
-  
-      },
-      {
-        title:"site",
-        url:"#site",
-    
-        },
-        {
-          title:"port",
-          url:"#port",
-      
-          },
-          {
-            title:"contact",
-            url:"#contact",
-        
-            },
-        
+        title: "skill",
+        url: "#skill"
+    },
+    {
+        title: "site",
+        url: "#site"
+    },
+    {
+        title: "portfolio",
+        url: "#port"
+    },
+    {
+        title: "contact",
+        url: "#contact"
+    },
 ]
+
 
 const Header = () => {
 
-  const [show,setShow] = useState(false);
-  const toggleMenu = () =>{
-    setShow((prevShow) => !prevShow);
-    // ! : 반대 ( 반대로 바꿔주는 명령어)
+    const [show, setShow] = useState(false);
+    const toggleMenu = () => {
+        setShow((prewShow) => !prewShow);
+    }
 
-  }
-  
-  
+    return (
+        <header id='header' role='banner'>
+            <div className="header_inner">
+                <div className="header_logo">
+                    <a href="/">portfolio <em>react</em></a>
+                </div>
+                <div className={`header_nav ${show ? "show" : ""}`}
+                    role='navigation'
+                    aria-label='메인 메뉴'
+                >
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
 
-
-  return (
-    <header id='header' role='banner'>
-      <div className="header_inner">
-        <div className="header_logo">
-          <a href="/">portfolio <em>react</em></a>
-        </div>
-        <div className={`header_nav ${show ? "show" :""}`} role='navigation' aria-label='메인 메뉴'>
-
-          <ul>
-
-            {headerNav.map((nav,key)=>(
-              <li key={key}>
-                <a href={nav.url}>{nav.title}</a>
-                </li>
-
-            ))}
-            
-
-
-
-            
-          </ul>
-        </div>
-        <div className="header_nav_moblie" id="headerToggle" aria-controls="primany-menu" aria-expanded={show ? "true":"false"} role='button' tabIndex="0" onClick={toggleMenu}>
-          <span></span>
-
-        </div>
-      </div>
-    </header>
-  )
+                    </ul>
+                </div>
+                <div
+                    className="header_nav_moblie"
+                    id='headerToggle'
+                    aria-expanded="false"
+                    aria-controls='primary-menu'
+                    role='button'
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
